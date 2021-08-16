10bed6d35276707ea4593fe279673d60d2e7118d
BrunoBig <bnbigliatto@gmail.com>
2021-08-16 18:29:53 -0300

    enem velho policy pass role

--- etl/02_delta_spark_upsert.py
+++ etl/02_delta_spark_upsert.py
@@ -87,7 +87,7 @@ enemnovo = enemnovo.withColumn("NO_MUNICIPIO_RESIDENCIA", lit("NOVA CIDADE")).wi
 
 
 logger.info("Pega os dados do Enem velhos na tabela Delta...")
-enemvelho = DeltaTable.forPath(spark, "s3://datalake-ney-igti-edc-tf/staging-zone/enem")
+enemvelho = DeltaTable.forPath(spark, "s3://datalake-bruno-468906988086-igti-edc-tf/staging-zone/enem")
 
 
 logger.info("Realiza o UPSERT...")
